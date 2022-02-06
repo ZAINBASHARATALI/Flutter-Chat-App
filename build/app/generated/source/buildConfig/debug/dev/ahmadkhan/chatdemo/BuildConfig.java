a5c24523698a3f1246344c47dc627e2d7c7491f9
Zain Basharat Ali <zainali112718@gmail.com>
2022-02-06 22:40:08 +0500

    first commit

---
+++ build/app/generated/source/buildConfig/debug/dev/ahmadkhan/chatdemo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dev.ahmadkhan.chatdemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dev.ahmadkhan.chatdemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
